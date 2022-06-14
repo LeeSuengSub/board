5a1bfa61268298bfba978f58f4a201f464f8f728
LeeSuengSub <zlzm2522@gmail.com>
2022-06-14 16:26:41 +0900

    feat: 페이징_테스트

--- src/test/java/com/dw/board/BoardApplicationTests.java
+++ src/test/java/com/dw/board/BoardApplicationTests.java
@@ -1,6 +1,5 @@
 package com.dw.board;
 
-
 import java.util.List;
 import java.util.Map;
 
@@ -16,54 +15,54 @@ class BoardApplicationTests {
 
 	@Autowired
 	private PageHandler pageHandler;
-	
+
 	@Autowired
 	private BoardMapper boardMapper;
-	
+
 	@Test
 	void contextLoads() {
-		
-		int total = boardMapper.selectAllBoardTotal(); //전체 게시물 수
-		System.out.println("total => "+total);
+
+		int total = boardMapper.selectAllBoardTotal(); // 전체 게시물 수
+		System.out.println("total => " + total);
 		int pageNum = 2; // 현재 페이지 번호
-		int pageSize = 10;//한 페이지에 게시물 10개
+		int pageSize = 10;// 한 페이지에 게시물 10개
 		int navigatePages = 5; // 한블록의 페이지 5개
-		
+
 		pageHandler.setTotal(total);
 		pageHandler.setPageNum(pageNum);
 		pageHandler.setPageSize(pageSize);
 		pageHandler.setNavigatePage(navigatePages);
-		
+
 		pageHandler.setNowBlock(pageNum);
-		int nowBlock = pageHandler.getNowBlock(); //현재 블록
-		System.out.println("현재 블록 => "+nowBlock);
-		
+		int nowBlock = pageHandler.getNowBlock(); // 현재 블록
+		System.out.println("현재 블록 => " + nowBlock);
+
 		pageHandler.setLastBlock(total);
 		int lastBlock = pageHandler.getLastBlock();
-		System.out.println("마지막 블록 =>"+ lastBlock);
-		
+		System.out.println("마지막 블록 =>" + lastBlock);
+
 		pageHandler.setStartPage(nowBlock);
 		int startPage = pageHandler.getStartPage();
-		System.out.println("현재 페이지 => "+startPage);
-		
+		System.out.println("현재 페이지 => " + startPage);
+
 		int pages = pageHandler.calcPage(total, pageSize);
-		pageHandler.setEndPage(nowBlock,pages);
+		pageHandler.setEndPage(nowBlock, pages);
 		int lastPage = pageHandler.getEndPage();
-		System.out.println("마지막 페이지 => "+ lastPage);
-		
+		System.out.println("마지막 페이지 => " + lastPage);
+
 		pageHandler.setPreNext(pageNum);
 		boolean hasPreviousPage = pageHandler.isHasPreviousPage();
 		boolean hasNextPage = pageHandler.isHasNextPage();
-		System.out.println("이전 버튼 유무 => "+ hasPreviousPage);
-		System.out.println("다음 버튼 유무 => "+ hasNextPage);
-		
-		int limitStart = ((pageNum-1) * pageSize);
-		List<Map<String, Object>> list = boardMapper.selectSearchBoardListTEST(limitStart,pageSize);
-		
-		for(Map<String, Object> param : list) {
+		System.out.println("이전 버튼 유무 => " + hasPreviousPage);
+		System.out.println("다음 버튼 유무 => " + hasNextPage);
+
+		int limitStart = ((pageNum - 1) * pageSize);
+		List<Map<String, Object>> list = boardMapper.selectSearchBoardListTEST(limitStart, pageSize);
+
+		for (Map<String, Object> param : list) {
 			String studentsName = (String) param.get("students_name");
 			System.out.println(studentsName);
 		}
-		
-		}
+
 	}
+}
